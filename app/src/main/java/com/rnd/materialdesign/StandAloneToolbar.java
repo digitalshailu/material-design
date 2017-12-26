f6439206ad359ac4161d30f086c14bf63bc3b706
shailendra.chotalia <digitalshailu>
2017-12-26 17:04:38 +0000

    Action item added on toolbar with menu

--- app/src/main/java/com/rnd/materialdesign/StandAloneToolbar.java
+++ app/src/main/java/com/rnd/materialdesign/StandAloneToolbar.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by shailendra.chotalia on 26/12/2017.
@@ -19,6 +20,23 @@ public class StandAloneToolbar extends AppCompatActivity {
         toolbar.setTitle("Standard Toolbar");
         toolbar.setSubtitle("sub title");
         toolbar.setNavigationIcon(R.drawable.ic_launcher);
+
+
+        //Inflate the menu and the click listener.
+        toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.save:
+                        break;
+                    case R.id.refresh:
+                        break;
+                }
+                return true;
+            }
+        });
 
     }
 }
